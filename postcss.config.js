f7cc15a781e09c3a61a7fd153ddfb6d1f339f103
Bart van Raaij <info@bartvanraaij.nl>
2020-08-26 19:05:22 +0200

    Fix build. Split up workflow jobs into build and deploy.

--- postcss.config.js
+++ postcss.config.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV === 'production') {
   // noinspection JSValidateTypes
   plugins.push(
     require('@fullhuman/postcss-purgecss')({
-      content: ['_site/**/*.html'],
+      content: ['src/**/*.njk', 'src/**/*.md'],
     }),
   );
   plugins.push(
